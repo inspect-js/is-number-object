7e6be2fd2346557fc81bd544ac8745021c50e266
Jordan Harband <ljharb@gmail.com>
2015-01-29 10:23:08 -0800

    Improve optimizability of the non-try/catch part.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var toStr = Number.prototype.toString;
-
-module.exports = function isNumberObject(value) {
-	if (typeof value === 'number') { return true; }
-	if (typeof value !== 'object') { return false; }
+var tryNumberObject = function tryNumberObject(value) {
 	try {
 		toStr.call(value);
 		return true;
 	} catch (e) {
 		return false;
 	}
+};
+
+module.exports = function isNumberObject(value) {
+	if (typeof value === 'number') { return true; }
+	if (typeof value !== 'object') { return false; }
+	return tryNumberObject(value);
 };
