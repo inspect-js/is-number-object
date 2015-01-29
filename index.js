eae43375d3f88e04bb10eabd954e5a6b66ad5305
Jordan Harband <ljharb@gmail.com>
2015-01-29 09:33:48 -0800

    Add early exits for typeof number, or typeof not "object".

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var toStr = Number.prototype.toString;
 
 module.exports = function isNumberObject(value) {
+	if (typeof value === 'number') { return true; }
+	if (typeof value !== 'object') { return false; }
 	try {
 		toStr.call(value);
 		return true;
