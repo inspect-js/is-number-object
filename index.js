834c09801d923ddf638585a94020b7c3b3cec3dc
Jordan Harband <ljharb@gmail.com>
2024-12-01 16:04:01 -0800

    [Robustness] use `call-bind`

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 'use strict';
 
-var numToStr = Number.prototype.toString;
+var callBound = require('call-bind/callBound');
+
+var $numToStr = callBound('Number.prototype.toString');
+
 var tryNumberObject = function tryNumberObject(value) {
 	try {
-		numToStr.call(value);
+		$numToStr(value);
 		return true;
 	} catch (e) {
 		return false;
 	}
 };
-var toStr = Object.prototype.toString;
+var $toString = callBound('Object.prototype.toString');
 var numClass = '[object Number]';
 var hasToStringTag = require('has-tostringtag/shams')();
 
@@ -20,5 +23,5 @@ module.exports = function isNumberObject(value) {
 	if (typeof value !== 'object') {
 		return false;
 	}
-	return hasToStringTag ? tryNumberObject(value) : toStr.call(value) === numClass;
+	return hasToStringTag ? tryNumberObject(value) : $toString(value) === numClass;
 };
